99ca839f522bc28b21d8a652747ab1f2298b1d49
izuhara2 <izuhara0221@gmail.com>
2022-12-27 13:17:39 +0900

    main

--- js/Shop.js
+++ js/Shop.js
@@ -30,7 +30,7 @@ class Shop {
          * @type {number}
          */
         this._number = number;
-        for (let i = 0; i < number; i++) this._registers.push( new Register(i) );
+        for (let i = 0; i < number; i++) this._registers.push(new Register(i));
     }
 
     /**
@@ -44,6 +44,17 @@ class Shop {
                 register.remain = remain;
             }
         }
+
+        /**客移動メソッド */
+        function move() {
+            const regi_enter = { //客が窓口でサービスを受ける座標
+                x: [60, 170, 280, 390, 500, 610, 720, 830, 940, 1050],
+                y: [650, 650, 650, 650, 650, 650, 650, 650, 650]
+            }
+            document.querySelector('#customer' + register_id).style.left = `${regi_enter.x[register_id]}px`;;
+            document.querySelector('#customer' + register_id).style.top = `${regi_enter.y[register_id]}px`;;
+        }
+
         for (let register of this._registers.sort((r1, r2) => {
             return r1.remain < r2.remain ? -1 : 1
         })) {
@@ -52,7 +63,11 @@ class Shop {
                 // 待ち行列に客がいるか？
                 if (this._list.getLength() > 0) {
                     // 客を移動（待ち行列→レジ）
-                    register.push(this._rew, this._list.shift());
+                    //register.push(this._rew, this._list.shift());
+                    let customer = this_list.shift();
+                    customer.move(register.id)
+                    this_list.move();
+                    register.push(this_rew, customer);
                 } else register.remain = 0;
             }
         }
@@ -60,3 +75,4 @@ class Shop {
         this._rew = this._list.process(time);
     }
 }
+process();
